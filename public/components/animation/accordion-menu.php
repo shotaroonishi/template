6458591a987aa1ad658584e398bcd113a4dc8054
shotaroonishi <o_onishi@kani-design.com>
2022-02-09 12:37:14 +0900

    改造

---
+++ public/components/animation/accordion-menu.php
@@ -0,0 +1,34 @@
+<dl class="accordion js-accordion">
+<div class="accordion__item js-accordion-trigger">
+<dt class="accordion__title">タイトル</dt>
+<dd class="accordion__content">DEMO1のコンテンツ</dd>
+</div>
+<div class="accordion__item js-accordion-trigger">
+<dt class="accordion__title">タイトル</dt>
+<dd class="accordion__content">DEMO1のコンテンツ</dd>
+</div>
+<div class="accordion__item js-accordion-trigger">
+<dt class="accordion__title">タイトル</dt>
+<dd class="accordion__content">DEMO1のコンテンツ</dd>
+</div>
+<div class="accordion__item js-accordion-trigger">
+<dt class="accordion__title">タイトル</dt>
+<dd class="accordion__content">DEMO1のコンテンツ</dd>
+</div>
+<div class="accordion__item js-accordion-trigger">
+<dt class="accordion__title">タイトル</dt>
+<dd class="accordion__content">DEMO1のコンテンツ</dd>
+</div>
+<div class="accordion__item js-accordion-trigger">
+<dt class="accordion__title">タイトル</dt>
+<dd class="accordion__content">DEMO1のコンテンツ</dd>
+</div>
+<div class="accordion__item js-accordion-trigger">
+<dt class="accordion__title">タイトル</dt>
+<dd class="accordion__content">DEMO1のコンテンツ</dd>
+</div>
+<div class="accordion__item js-accordion-trigger">
+<dt class="accordion__title">タイトル</dt>
+<dd class="accordion__content">DEMO1のコンテンツ</dd>
+</div>
+</dl>
---
+++ public/layout/script-list.php
@@ -0,0 +1,1 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script type="text/javascript" src="/js/common.js"> </script>
