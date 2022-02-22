e96ae81e0c7e420018d2248731ffc90f8402dbd3
shotaroonishi <o_onishi@kani-design.com>
2022-02-22 14:24:15 +0900

    プラグイン使い

---
+++ src/js/animation/slick.js
@@ -0,0 +1,18 @@
+
+$(".slick01").slick({
+  autoplay: true, // 自動再生を設定
+  autoplaySpeed: 2000, // スライド切り替えの時間を設定
+  dots: true, // ドットインジケーターの表示
+  infinite: true, // スライドのループを有効にするか
+  slidesToShow: 3, // 表示するスライド数を設定
+  slidesToScroll: 1, // スクロールするスライド数を設定
+  responsive: [
+    {
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
