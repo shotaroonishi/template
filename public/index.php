deb0d8e3d05ce6dd1c76165289d454c807dab94d
shotaroonishi <o_onishi@kani-design.com>
2022-02-25 13:41:54 +0900

    aos

--- public/index.php
+++ public/index.php
@@ -2,15 +2,15 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 <meta name="format-detection" content="telephone=no">
-<title>ページのタイトル</title>
-<meta name="description" content="ページの説明文">
+<title>ページ名</title>
+<meta name="description" content="ディスクリプション">
 <meta name="keywords" content="キーワード">
-<link rel="shortcut icon" href="/favicon.ico">
+<link rel="shortcut icon" href="/img/favicon.ico">
 <link rel="apple-touch-icon-precomposed" href="タッチアイコン画像へのパス">
-<meta property="og:url" content="ページのURL">
-<meta property="og:type" content="ページの種類">
-<meta property="og:title" content="ページのタイトル">
-<meta property="og:description" content="ページの説明文">
-<meta property="og:site_name" content="サイト名">
-<meta property="og:image" content="サムネイル画像のURL">
-<meta name="twitter:card" content="summary_large_image"><link rel="stylesheet" href="/css/common.min.css"><title>タイトル</title></head><body id="js-drawer"> <div class="openbtn-wrap pc-none"><div class="openbtn" id="js-btn"><span> </span><span> </span><span> </span></div></div><nav class="pc-none" id="g-nav"> <div id="g-nav-list"><ul> <li> <a href="" target="">テキスト</a></li><li> <a href="" target="">テキスト</a></li><li> <a href="" target="">テキスト</a></li></ul></div></nav><header class="header"><div class="logo">  <a href=""> </a></div><nav class="header__nav"> <ul> <li> <a href=""> </a></li></ul></nav></header><main class="main__wrap"></main><footer class="footer"></footer><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script type="text/javascript" src="/js/common.js"> </script></body></html>
+<meta property="og:url" content="url">
+<meta property="og:type" content="website">
+<meta property="og:title" content="ページ名">
+<meta property="og:description" content="ディスクリプション">
+<meta property="og:site_name" content="ページ名">
+<meta property="og:image" content="/img/">
+<meta name="twitter:card" content="summary_large_image"><link rel="stylesheet" href="/css/common.min.css"></head><body id="js-drawer"> <div class="openbtn-wrap pc-none"><div class="openbtn" id="js-btn"><span> </span><span> </span><span> </span></div></div><nav class="pc-none" id="g-nav"> <div id="g-nav-list"><ul> <li> <a href="" target="">テキスト</a></li><li> <a href="" target="">テキスト</a></li><li> <a href="" target="">テキスト</a></li></ul></div></nav><header class="header"></header><main class="main__wrap"></main><footer class="footer"></footer><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script type="text/javascript" src="/js/common.js"> </script></body></html>
