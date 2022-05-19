bcbe21ae46a21230207db7f0c516d25e287dd9ab
shotaroonishi <o_onishi@kani-design.com>
2022-05-19 16:34:37 +0900

    meta_dateページ識別

--- public/index.php
+++ public/index.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
-<meta name="format-detection" content="telephone=no">
-<title>ページ名</title>
-<meta name="description" content="ディスクリプション">
-<meta name="keywords" content="キーワード">
-<link rel="shortcut icon" href="/img/favicon.ico">
-<link rel="apple-touch-icon-precomposed" href="タッチアイコン画像へのパス">
-<meta property="og:url" content="url">
-<meta property="og:type" content="website">
-<meta property="og:title" content="ページ名">
-<meta property="og:description" content="ディスクリプション">
-<meta property="og:site_name" content="ページ名">
-<meta property="og:image" content="/img/">
-<meta name="twitter:card" content="summary_large_image"><link rel="stylesheet" href="/css/common.min.css"><link rel="preconnect" href="https://fonts.googleapis.com">
+<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"><title>タイトル</title><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="description" content="デスクリプション"><meta name="keywords" content="キーワード"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon"><meta property="og:url" content="url"><meta property="og:type" content="website"><meta property="og:title" content="タイトル"><meta property="og:description" content="デスクリプション"><meta property="og:image" content="/img/"><meta name="twitter:card" content="summary_large_image"><link rel="stylesheet" href="/css/common.min.css"><link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet"></head><body id="js-drawer"> <div class="openbtn-wrap pc-none"><div class="openbtn" id="js-btn"><span> </span><span> </span><span> </span></div></div><nav class="pc-none" id="g-nav"> <div id="g-nav-list"><ul> <li> <a href="" target="">テキスト</a></li><li> <a href="" target="">テキスト</a></li><li> <a href="" target="">テキスト</a></li></ul></div></nav><header class="header"></header><main class="main__wrap"></main><footer class="footer"></footer><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script type="text/javascript" src="/js/common.js"> </script></body></html>
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet"></head><body id="js-drawer"> <div class="openbtn-wrap pc-none"><div class="openbtn" id="js-btn"><span> </span><span> </span><span> </span></div></div><nav class="pc-none" id="g-nav"> <div id="g-nav-list"><ul> <li> <a href="" target="">テキスト</a></li><li> <a href="" target="">テキスト</a></li><li> <a href="" target="">テキスト</a></li></ul></div></nav><header class="header"></header><main><p>トップページのコンテンツエリアです</p></main><footer class="footer"></footer><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script><script type="text/javascript" src="/js/common.js"> </script></body></html>
